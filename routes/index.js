fb42bf99aafac0bad10e8fb8af5f26f06ec80f99
Lawrence Rivales <lrivales@gmail.com>
2021-12-09 11:32:34 -0500

    added /api/pizzas route and connected to front-end

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+// import all of the api routes from /api/index.js
+const apiRoutes = require('./api');
+const htmlRoutes = require('./html/html-routes');
+
+router.use('/api', apiRoutes);
+router.use('/', htmlRoutes);
+
+router.use((req, res) => {
+  res.status(404).send('<h1>😝 404 Error!</h1>');
+});
+
+module.exports = router;
